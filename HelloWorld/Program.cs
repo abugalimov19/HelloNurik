b24996ee6bdd4037987e155d88d19d71b216c7df
Abugalimov <abugalimov19@mail.ru>
2020-12-15 14:57:41 +0300

    Начальный возврат модуля HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"[{DateTime.Now}] Today was a good day, {name}!)");
+            Console.ReadLine();
+        }
+    }
+}
